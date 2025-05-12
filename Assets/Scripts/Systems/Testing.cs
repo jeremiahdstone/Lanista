e64c408bd10fb26458e64d545d5d170d1bb18762
KAMISENHEIMER <therealspydude@txstate.edu>
2025-05-12 16:24:48 -0500

    Adding testing file and game object to test code.

---
+++ Assets/Scripts/Systems/Testing.cs
@@ -0,0 +1,12 @@
+using UnityEngine;
+
+//for making sure all the scripts work correctly
+public class Testing : MonoBehaviour
+{
+    void Start()
+    {
+        Debug.Log(new Gladiator());
+        Debug.Log(new Gladiator());
+        Debug.Log(new Gladiator());
+    }
+}
